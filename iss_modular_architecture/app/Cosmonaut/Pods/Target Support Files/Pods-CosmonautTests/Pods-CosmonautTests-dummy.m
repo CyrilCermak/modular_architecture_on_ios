460ca1fcf016ddf5e9029b9d6f3a5a7424ad75f7
Cyril Cermak <cyril.cermak@gmail.com>
2024-06-19 23:01:21 +0200

    Removing pods from git

--- iss_modular_architecture/app/Cosmonaut/Pods/Target Support Files/Pods-CosmonautTests/Pods-CosmonautTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CosmonautTests : NSObject
-@end
-@implementation PodsDummy_Pods_CosmonautTests
-@end
--- iss_modular_architecture/app/Cosmonaut/Pods/Target Support Files/Pods-ISSUIComponents/Pods-ISSUIComponents-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ISSUIComponents : NSObject
-@end
-@implementation PodsDummy_Pods_ISSUIComponents
-@end
